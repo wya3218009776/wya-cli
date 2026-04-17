402293bee06a57eaf4176c7c2433351aa31eac47
wya3218009776 <3218009776@qq.com>
2026-04-17 18:21:26 +0800

    fix: stabilize ci publish workflow

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
 const importLocal = require('import-local');
 
 if (importLocal(__filename)) {
-  require("npmlog").info("cli", "正在使用 zt-cli 的本地版本");
+  require('npmlog').info('cli', '正在使用 wya-cli 的本地版本');
 } else {
-  require("../lib/core")(process.argv.slice(2));
+  require('../lib/core')(process.argv.slice(2));
 }
